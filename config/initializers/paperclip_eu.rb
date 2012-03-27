f4d0ef1ef1448a2a438e92091a7501e6e9c31105
nekora <n3k0rA@hotmail.com>
2012-03-27 22:23:29 +0100

    eu_bucket05

--- config/initializers/paperclip_eu.rb
+++ config/initializers/paperclip_eu.rb
@@ -5,6 +5,7 @@ Paperclip.interpolates(:s3_eu_url) do |att, style|
     "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
 end
 
+/
 module AWS
     module S3
         DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
