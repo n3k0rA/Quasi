e91f2fc0fcb954f924794db3eedd57043d17256c
nekora <n3k0rA@hotmail.com>
2012-03-27 17:57:21 +0100

    some_fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" },
     :storage => :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
-    :s3_credential => {
+    :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
