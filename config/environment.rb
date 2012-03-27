16f51b2949a136cf5b13d4af55a8ccc7500c9ecc
nekora <n3k0rA@hotmail.com>
2012-03-27 22:13:51 +0100

    eu_bucket

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 Quasi::Application.initialize!
 
 #Needed to run S3 in European servers
-AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+#AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
