196a8947a0b32fc7e81d45f620523b47d4eee958
WolaS <jcpenche@gmail.com>
2012-04-10 20:38:00 +0200

    photos and various other bits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,22 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  attr_accessor :photo_file_name
-  attr_accessor :photo_content_type
-  attr_accessor :photo_file_size
-  attr_accessor :photo_updated_at
-  attr_accessor :photo
   
   validates_presence_of :email
   validates_presence_of :name
   has_and_belongs_to_many :events
   has_many :created_events, :class_name => "Event", :foreign_key => "user_id"
   has_many :comments, :dependent => :destroy
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :storage => Rails.env.production? ? :s3 : :filesystem,
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    },
-  #  :s3_protocol => "https",
-  #  :path => ":class/:id/:basename_:style.:extension",
-    :path => ":attachment/:id/:style.:extension",
-    :bucket => ENV['S3_BUCKET_NAME']
-  #  :url  => ":s3_eu_url"
+  
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    #,
+    #:storage => Rails.env.production? ? :s3 : :filesystem,
+    #:s3_credentials => { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+    #:s3_protocol => "https",
+    #:path => ":class/:id/:basename_:style.:extension",
+    #:path => ":attachment/:id/:style.:extension",
+    #:bucket => ENV['S3_BUCKET_NAME']
+    #:url  => ":s3_eu_url"
+  
   has_many :followings, :dependent => :destroy
   has_many :followeds, :through => :followings, :dependent => :destroy
   has_many :communications
